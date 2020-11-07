252d55720870e245d7f399028de248601e559533
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2020-11-07 09:53:46 +0700

    done

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        int size;
+        int[] arr;
+
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Enter a size:");
+            size = sc.nextInt();
+            if (size > 20)
+                System.out.println("Size does not exceed 20");
+        } while (size > 20);
+
+        arr = new int[size];
+        int i = 0;
+
+        while (i < arr.length) {
+
+            System.out.print("Enter number in array: ");
+            int val = sc.nextInt();
+            arr[i] = val;
+            i++;
+        }
+        System.out.printf("%-20s%s", "Elements in array: ", "");
+        for (int j = 0; j < arr.length; j++) {
+            System.out.print(arr[j] + "\t");
+        }
+
+    }
 }
